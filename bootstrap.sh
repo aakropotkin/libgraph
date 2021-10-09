69a37e7d3ce2f7124e19c3b65c7c55c7f82dfa7d
aakropotkin <alex.ameen.tx@gmail.com>
2021-10-09 18:04:57 -0500

    gnulib build seems to work, passes all tests

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,28 @@
 #! /usr/bin/env sh
 mkdir -p m4;
 
-##declare -a GNULIB_MODULES=(
-##  list
-##  map
-##  realloc-gnu
-##  set
-##  strdup-posix
-##  strndup
-##);
-##
-##gnulib-tool                      \
-##  --libtool                      \
-##  --with-tests                   \
-##  --no-conditional-dependencies  \
-##  --vc-files                     \
-##  --macro-prefix=gl              \
-##  --source-base=gnulib           \
-##  --m4-base=gnulib/m4            \
-##  --doc-base=gnulib/doc          \
-##  --tests-base=gnulib/tests      \
-##  --aux-dir=build-aux            \
-##  --import                       \
-##  ${GNULIB_MODULES[*]}           \
-##  ;
+declare -a GNULIB_MODULES=(
+  list
+  map
+  realloc-gnu
+  set
+  strdup-posix
+  strndup
+);
+
+gnulib-tool                      \
+  --libtool                      \
+  --with-tests                   \
+  --no-conditional-dependencies  \
+  --vc-files                     \
+  --macro-prefix=gl              \
+  --source-base=gnulib           \
+  --m4-base=gnulib/m4            \
+  --doc-base=gnulib/doc          \
+  --tests-base=gnulib/tests      \
+  --aux-dir=build-aux            \
+  --import                       \
+  ${GNULIB_MODULES[*]}           \
+  ;
 
 aclocal && autoreconf -if;
