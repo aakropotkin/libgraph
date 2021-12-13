afdb5f327a2507e40d1e168e60f66571e15b0fb8
Alex Ameen <alex.ameen.tx@gmail.com>
2021-12-12 21:15:46 -0600

    add fake setup hook

--- default.nix
+++ default.nix
@@ -1,8 +1,17 @@
-{ stdenv, autoreconfHook }:
+{ stdenv, autoreconfHook, writeText }:
 stdenv.mkDerivation {
   pname = "libgraph";
   version = "0.1.0";
   src = ./.;
   nativeBuildInputs = [autoreconfHook];
   doCheck = true;
+  setupHook = writeText "setupHook.sh" ''
+
+    setLibgraphEnv() {
+      LIBGRAPH_PATH=$i
+      export LIBGRAPH_PATH
+    }
+
+    addEnvHooks "$targetOffset" addLibgraphEnv
+  '';
 }
