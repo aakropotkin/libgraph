9420a5a3ee98480d61e56535eaf9e6e00a53db3a
Alex Ameen <alex.ameen.tx@gmail.com>
2021-12-23 01:03:10 -0600

    added missing EXTRA_DIST files

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
-{ stdenv, autoreconfHook, srcTarball ? null }:
-stdenv.mkDerivation {
+{ stdenv, releaseTools }:
+let
+  srcTarball = import ./release.nix { inherit (releaseTools) sourceTarball; };
   pname = "libgraph";
   version = "0.1.0";
-  src = srcTarball or ./.;
+in stdenv.mkDerivation {
+  inherit pname version;
+  src = "${srcTarball}/tarballs/${pname}-${version}.tar.gz";
   doCheck = true;
 }
